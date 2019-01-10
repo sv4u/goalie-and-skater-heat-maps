eb4432faf89479f752f99576e092c59cd87751a0
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2019-01-10 13:25:51 -0500

    starting analysis. updated render script

--- heatmap.R
+++ heatmap.R
@@ -1,7 +1,7 @@
 library(rmarkdown)
 
-render("heatmap.Rmd", output_format = "html_document", output_dir = "docs");
+render("heatmap.Rmd", output_format = "html_document", output_dir = "docs", quiet = TRUE);
 print("HTML file has been rendered")
 
-render("heatmap.Rmd", output_format = "pdf_document", output_dir = "docs");
+render("heatmap.Rmd", output_format = "pdf_document", output_dir = "docs", quiet = TRUE);
 print("PDF document has been rendered")
